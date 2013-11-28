9d90b19bfb80cc7f51c13e35c19087472d57ca95
Jsawrus <j.s.walsh@icloud.com>
2013-11-28 00:44:54 +0000

    Starting the Plugin

---
+++ src/com/ftbmasters/Masters.java
@@ -0,0 +1,14 @@
+package com.ftbmasters;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Masters extends JavaPlugin {
+	
+	protected Plugin plugin;
+	
+	public void onEnable() {
+		this.plugin = this;
+	}
+
+}
