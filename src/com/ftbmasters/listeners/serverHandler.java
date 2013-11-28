e17144cc073a11c8d811473a878988b4df8de672
Jsawrus <j.s.walsh@icloud.com>
2013-11-28 01:20:47 +0000

    ServerPingEvent

--- src/com/ftbmasters/listeners/serverHandler.java
+++ src/com/ftbmasters/listeners/serverHandler.java
@@ -1,13 +1,24 @@
 package com.ftbmasters.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.Plugin;
 
 public class serverHandler implements Listener {
 	
 	public serverHandler(Plugin pl) {
 		Bukkit.getPluginManager().registerEvents(this, pl);
+	}
+	
+	@EventHandler (priority = EventPriority.NORMAL)
+	public void server(final ServerListPingEvent evt) {
+		evt.setMotd(ChatColor.ITALIC + "" + ChatColor.AQUA + "Kill Greg!");
+		// max players is 69
+		evt.setMaxPlayers((int) (((((6^2 * 10) + Math.sqrt((5000*3) - 600)) / 4 ) * 4 ) - Math.log((1 * 10^11))));
 	}
 
 }
