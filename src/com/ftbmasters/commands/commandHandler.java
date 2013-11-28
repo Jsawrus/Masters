51950bf4f9c442b341faacec24cd0c8c67ae9678
Jsawrus <j.s.walsh@icloud.com>
2013-11-28 01:00:39 +0000

    Temporary Command Handling

--- src/com/ftbmasters/commands/commandHandler.java
+++ src/com/ftbmasters/commands/commandHandler.java
@@ -1,14 +1,31 @@
 package com.ftbmasters.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class commandHandler implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
+		if (cmd.getName().equalsIgnoreCase("masters")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Make sure commands are sent from in-game."); 
+				return false;
+			}
+			
+			Player player = (Player) sender;
+			
+			if (player.hasPermission("masters.plugin.op")) {
+				// for lack of anything useful at this time..
+				player.sendMessage(ChatColor.GREEN + "This server is running " + Bukkit.getVersion() + ".");
+			} else {
+				player.sendMessage(ChatColor.RED + "Sorry, you lack sufficient privileges to perform this action.");
+			}
+		}
 		return false;
 	}
 
