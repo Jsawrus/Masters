b9a42899534c318a0dd2cdd0307d164aab54ce23
Jsawrus <j.s.walsh@icloud.com>
2013-11-30 21:17:31 +0000

    Invite Command

--- src/com/ftbmasters/Masters.java
+++ src/com/ftbmasters/Masters.java
@@ -1,13 +1,19 @@
 package com.ftbmasters;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import com.ftbmasters.IO.fileHandler;
 import com.ftbmasters.commands.commandHandler;
 import com.ftbmasters.commands.reloadHandler;
 import com.ftbmasters.commands.slapCommand;
 import com.ftbmasters.commands.tellHandler;
-import com.ftbmasters.listeners.*;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+import com.ftbmasters.listeners.TagWorker;
+import com.ftbmasters.listeners.blockHandler;
+import com.ftbmasters.listeners.playerHandler;
+import com.ftbmasters.listeners.serverHandler;
+import com.ftbmasters.listeners.teleportHandler;
+import com.ftbmasters.listeners.worldHandler;
 
 public class Masters extends JavaPlugin {
 
@@ -36,6 +42,7 @@ public class Masters extends JavaPlugin {
 		getCommand("tell").setExecutor(new tellHandler());
 		getCommand("slap").setExecutor(new slapCommand());
 		getCommand("reload").setExecutor(new reloadHandler());
+		getCommand("invite").setExecutor(new inviteCommand());
 	}
 
 	public static void fileHandlers() {
