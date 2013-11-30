fc50fb62f6754d8e41b77926fa8ea1342c3e9d61
Jsawrus <j.s.walsh@icloud.com>
2013-11-30 19:37:07 +0000

    Mossy Tweaks

--- src/com/ftbmasters/listeners/blockHandler.java
+++ src/com/ftbmasters/listeners/blockHandler.java
@@ -1,7 +1,12 @@
 package com.ftbmasters.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 public class blockHandler implements Listener {
@@ -10,4 +15,17 @@ public class blockHandler implements Listener {
 		Bukkit.getPluginManager().registerEvents(this, pl);
 	}
 
+	@SuppressWarnings("deprecation")
+	@EventHandler (priority = EventPriority.NORMAL)
+	public void mossify(final PlayerInteractEvent evt) {
+		if (evt.getPlayer() != null) {
+			if (evt.getClickedBlock().getType().equals(Material.COBBLESTONE)) {
+				if (evt.getPlayer().getItemInHand().getTypeId() == 1944) {
+					evt.getClickedBlock().setType(Material.MOSSY_COBBLESTONE);
+					evt.getPlayer().getInventory().remove(new ItemStack(1944, 1));
+				}
+			}
+		}
+	}
+	
 }
