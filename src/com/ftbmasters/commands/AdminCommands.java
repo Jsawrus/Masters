8f31d4343ca8c31d15f9d55d12de07f89c121ab2
aliem <lorenzo@frenzart.com>
2013-12-12 19:20:51 +0100

    almost useless worlds command

--- src/com/ftbmasters/commands/AdminCommands.java
+++ src/com/ftbmasters/commands/AdminCommands.java
@@ -2,7 +2,12 @@ package com.ftbmasters.commands;
 
 import com.ftbmasters.Masters;
 import com.ftbmasters.utils.commands.Command;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+
+import java.util.List;
 
 public class AdminCommands {
 	@Command (
@@ -15,6 +20,25 @@ public class AdminCommands {
 					"reloads configuration files."
 	)
 	public void masters(CommandSender sender, String[] args) {
+        sender.sendMessage(ChatColor.GOLD + "Reloading configuration files...");
 		Masters.fileHandlers();
 	}
+
+    @Command (
+            name = "worlds",
+            aliases = {},
+            player = false,
+            permission = "masters.admin",
+            usage = "/<command>",
+            description = "List Bukkit worlds"
+    )
+    public void worldList(CommandSender sender, String[] args) {
+        String r = ChatColor.AQUA + "";
+        for (World w: Bukkit.getServer().getWorlds()) {
+            r += w.getName() + ChatColor.RED + "("+ ChatColor.DARK_AQUA + w.getPlayers().size() + ChatColor.RED + ") " + ChatColor.AQUA;
+        }
+
+        sender.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "World List:");
+        sender.sendMessage(r + ChatColor.RESET);
+    }
 }
