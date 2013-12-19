f43018dc5b78eeef2b81946af5321f6bcf769b11
aliem <lorenzo@frenzart.com>
2013-12-19 12:04:47 +0100

    command to display a little help text for colors

--- src/com/ftbmasters/commands/SillyCommands.java
+++ src/com/ftbmasters/commands/SillyCommands.java
@@ -53,14 +53,14 @@ public class SillyCommands {
     }
 
     @Command (
-            name = "color",
+            name = "nameplate",
             aliases = {},
-            permission = "masters.color",
+            permission = "masters.nameplate",
             usage = "/<command> [color]",
             description = "Change your color! choose from: 1,2,3,4,5,6,7,9,a,b,c,d,e",
             player = true
     )
-    public void color(CommandSender sender, String[] args) {
+    public void nameplate(CommandSender sender, String[] args) {
         Masters plugin = (Masters) Masters.getInstance();
 
         if (!args[0].matches("[1-79a-e]")) {
@@ -68,5 +68,29 @@ public class SillyCommands {
         }
         plugin.getConfig().set("nameplates.players." + sender.getName(), args[0]);
         plugin.getNameplateManager().refreshNameplate(sender.getServer().getPlayer(sender.getName()));
+    }
+
+    @Command(
+            name = "colors",
+            aliases = {},
+            permission = "masters.colors",
+            usage = "/colors",
+            description = "List all the color codes you can use",
+            player = true
+    )
+    public void colors(CommandSender sender, String[] args) {
+        sender.sendMessage(new String[]{
+                ChatColor.BLACK + "&0" + ChatColor.DARK_BLUE + "&1" + ChatColor.DARK_GREEN + "&2",
+                ChatColor.DARK_AQUA + "&3" + ChatColor.DARK_RED + "&4" + ChatColor.DARK_PURPLE + "&5",
+                ChatColor.GOLD + "&6" + ChatColor.GRAY + "&7" + ChatColor.DARK_GRAY + "&8",
+                ChatColor.BLUE + "&9" + ChatColor.GREEN + "&a" + ChatColor.AQUA + "&b",
+                ChatColor.RED + "&c" + ChatColor.LIGHT_PURPLE + "&d" + ChatColor.YELLOW + "&e",
+                ChatColor.WHITE + "&f",
+                "&r resets color to the default.",
+                ChatColor.GOLD + " ~~ # ~~~~~~ ",
+                "&6 Gold Text &c Red Text &r default text",
+                "becomes: ",
+                ChatColor.translateAlternateColorCodes('&', "&6 Gold Text &c Red Text &r default text")
+        });
     }
 }
